010cd28df6086c1aa193d787d88a9493bf00ed37
Ryan Kielbasa <ryankielbasa@ryannumberone.local>
2026-04-29 12:24:02 -0500

    fix icon paths

--- vite.config.ts
+++ vite.config.ts
@@ -16,8 +16,8 @@ export default defineConfig({
         display: 'standalone',
         orientation: 'landscape',
         icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' },
+          { src: '/Workout/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/Workout/icon-512.png', sizes: '512x512', type: 'image/png' },
         ],
       },
       workbox: {
